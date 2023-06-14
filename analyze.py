1d6c1d1671b1c4547f98e974656647b34e653630
antoniocaruso <antonio.mario.caruso@gmail.com>
2023-06-14 17:37:59 +0200

    Added check for valid schedule.

--- analyze.py
+++ analyze.py
@@ -1,39 +1,11 @@
-
 # Please add here the date of last change, or add to a git repo.
-# Antonio / 27/04/2023
-
+# Antonio - 14/06/2023
 
 import sys
 import numpy as np
 from scheduling import Task,iot_schedule_exact,carfagna_schedule
 
-# def iterated_test():
-#     K: int = 24
-#     Bmin: int = 10
-#     Bmax: int = 220
-#     Bstart = 180
-#     Panel = [0,0,0,0,0,1,3,5,20,50,70,120,120,70,50,20,5,3,1,0,0,0,0,0]
-#     Tasks = []
-#     for x in zip([2,10,20,32,50],[1,2,3,4,5]):
-#         Tasks.append(Task(*x))
-#     (S,Q) = ScheduleClassic(K,Bstart,Bmin,Bmax,Panel,Tasks)
-#     print("Q = ",Q)
-#     print("S = ",S)
-#     qsum = 0
-#     Bend = Bstart
-#     S = []
-#     for i in range(K):
-#     #print("run with",(K-i,Bstart))
-#         (Q,task) = ScheduleNoMem(K-i,Bstart,Bmin,Bmax,Panel[i:],Tasks,Bend)
-#     qsum += Tasks[task-1].quality
-#     print("Q = ",Q," t = ",task-1, "qsum = ", qsum);
-#     Bstart = min( Bstart - Tasks[task-1].cost + Panel[i], Bmax )
-#     S += [task-1]
-#     print("S' =",S)
-#     print("TotalEnergy = ",sum(Panel)," ",sum(Panel)/K)
-#     sys.exit();
-
-#### main program
+np.set_printoptions(linewidth=100)
 
 def parse_input(lines):
     iterations = []
@@ -80,18 +52,18 @@ def parse_input(lines):
         i = j
     return i,com,iterations
 
-
+def check(E,S,Bstart,Bmin,Bmax,Cost,Quality):
+    k = len(S)
+    battery = Bstart
+    q = 0
+    for i in range(k):
+        battery = min(battery-Cost[S[i]-1]+E[i],Bmax)
+        q += Quality[S[i]-1]
+        if battery < Bmin: print("bmin violated:",battery)
+    if battery < Bstart:
+        print("Not Energy Neutral:", battery)
 
 if __name__ == "__main__":
-    # analyze.py console_output.txt option
-    # console_output.txt -> output from Ardino serial, start with #------
-    # option:
-    # none -> parse input, run schedule exact, compare with schedule Arduino
-    # 1.   -> parse input, just ouput the Arduino 'input' for scheduling
-    # 2.   -> parse input, run ScheduleClassic, output result (no comparison)
-    # 3.   -> parse input, run ScheduleCalssic and schedule, compare.
-    # 4.   -> parse input, check if quality is correct
-
     K,N,BMIN,BMAX,BINIT,BSAMPLING,MAX_QUALITY_LVL = [0]*7
     c_i: list[int] = []
     q_i: list[int] = []
@@ -135,40 +107,16 @@ if __name__ == "__main__":
         Energy = data['Energy']
         E = data['E']
         S = data['S']
-        print(f"iteration: {it}, E = {E}")
+        print(f"iteration: {it}")
+        print("E = ", E)
         (s1,quality1) = iot_schedule_exact(K,BINIT,BMIN,BMAX,E,Tasks)
         (s2,quality2) = carfagna_schedule(K,BINIT,BMIN,BMAX,MAX_QUALITY_LVL,E,Tasks)
-        print(f"quality input {alg_input}    =  {Q}")
-        print("quality python exact      = ",quality1)
-        print("quality python carfagna   = ",quality2)
-        print(f"S input {alg_input} = {S}")
-        print(f"S python exact    = {np.array(s1)+1}")
-        print(f"S python carfagna = {np.array(s2)+1}")
-
-
-        # do not use for now..
-        # if option == 1:
-        #     print("%2d" % (it),"\t",E)
-        # elif option == 2:
-        #     print("%2d" % (it),"\t",E)
-        #     (s,q) = ScheduleClassic(K,BINIT,BMIN,BMAX,E,Tasks)
-        #     print(" ",q,end=" - ")
-        #     print(f"S = {s}" if q!=0 else "")
-        # elif option == 3:
-        #     print("%2d" % (it),"\t",E)
-        #     (s,q) = ScheduleClassic(K,BINIT,BMIN,BMAX,E,Tasks)
-        #     print(" ",q,end=" - ")
-        #     print(f"S = {s}" if q!=0 else "")
-        #     (s,q) = schedule(K,BINIT,BMIN,BMAX,E,Tasks)
-        #     print(" ",q,end=" - ")
-        #     print(f"S = {s}" if q!=0 else "")
-        # elif option == 0:   
-        #     print("%2d " % (it),Energy,Time,end=" - \n")
-        #     (Sn,q) = schedule(K,BINIT,BMIN,BMAX,E,Tasks)
-        #     print("\t",E)
-        #     print("\tS Esatta   ",Sn,q)
-        #     print("\tS Arduino  ",S,Q,"%.1f%%" % (100*Q/q))
-        # elif option == 4:
-        #     print("%2d" % (it)," ",E)
-        #     (s,q) = ScheduleClassic(K,BINIT,BMIN,BMAX,E,Tasks)
-        #     print("    ",S,Q,check(K,S,BINIT,BMIN,BMAX,E,Tasks),q)
+        print(f"quality input {alg_input}    =\t  {Q}")
+        print("quality python exact =\t ",quality1)
+        if alg_input=='Carfana':
+            print("quality python carfagna   = ",quality2)
+        print(f"S input {alg_input}\t= {S}")
+        print(f"S python exact\t= {np.array(s1)+1}")
+        if alg_input=='Carfana':
+            print(f"S python carfagna\t= {np.array(s2)+1}")
+        check(E,S,BINIT,BMIN,BMAX,c_i,q_i)
